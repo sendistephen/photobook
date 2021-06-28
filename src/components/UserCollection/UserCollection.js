ce50433a4bdd558d1500a2d67428cf7188660091
sendistephen <sendi.steve2@gmail.com>
2021-06-28 23:03:04 +0300

    Add feature for getting user collection

---
+++ src/components/UserCollection/UserCollection.js
@@ -0,0 +1,69 @@
+import axios from 'axios';
+import { Component } from 'react';
+
+import { getUserCollections } from 'utils/api';
+import {
+  Collection,
+  StyledSlider,
+  CollectionBox,
+  Cover,
+  Title,
+} from './UserCollection.styles';
+
+class UserCollection extends Component {
+  state = {
+    collections: [],
+    page: 1,
+    per_page: 20,
+    isLoading: false,
+  };
+
+  componentDidMount = () => {
+    const { username } = this.props;
+    this.getCollection(username);
+  };
+
+  getCollection = async (username) => {
+    try {
+      this.setState({ isLoading: false });
+      const url = getUserCollections({
+        username: username,
+        page: this.state.page,
+        per_page: this.state.per_page,
+      });
+      const res = await axios(url);
+      const data = res.data;
+      this.setState({ collections: [...this.state.collections, ...data] });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  render() {
+    console.log(this.state.collections);
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+    };
+    return (
+      <Collection>
+        <StyledSlider {...settings}>
+          {this.state.collections.map((collection) => (
+            <CollectionBox key={collection.id}>
+              <Cover
+                src={collection.cover_photo.urls.small}
+                alt={collection.title}
+              />
+              <Title>{collection.title}</Title>
+            </CollectionBox>
+          ))}
+        </StyledSlider>
+      </Collection>
+    );
+  }
+}
+
+export default UserCollection;
