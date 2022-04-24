a40b435322ce1e3e63bb39a191535d483a14e2aa
sendistephen <sendi.steve2@gmail.com>
2022-04-24 05:17:22 +0300

    Fix infinite scroll issue on user page

--- src/store/user/userReducer.js
+++ src/store/user/userReducer.js
@@ -1,104 +1,104 @@
 import {
-  USER_FETCH_USER_ERROR,
-  USER_FETCH_USER_PENDING,
-  USER_FETCH_USER_SUCCESS,
-  USER_FETCH_USER_PHOTOS_ERROR,
-  USER_FETCH_USER_PHOTOS_PENDING,
-  USER_FETCH_USER_PHOTOS_SUCCESS,
-  USER_FETCH_USER_COLLECTIONS_SUCCESS,
-  USER_FETCH_USER_COLLECTIONS_PENDING,
-  USER_FETCH_USER_COLLECTIONS_ERROR,
-  USER_CLEAR_USER_PHOTOS,
-  OPEN_MODAL,
-} from './userTypes';
+	USER_FETCH_USER_ERROR,
+	USER_FETCH_USER_PENDING,
+	USER_FETCH_USER_SUCCESS,
+	USER_FETCH_USER_PHOTOS_ERROR,
+	USER_FETCH_USER_PHOTOS_PENDING,
+	USER_FETCH_USER_PHOTOS_SUCCESS,
+	USER_FETCH_USER_COLLECTIONS_SUCCESS,
+	USER_FETCH_USER_COLLECTIONS_PENDING,
+	USER_FETCH_USER_COLLECTIONS_ERROR,
+	USER_CLEAR_USER_PHOTOS,
+	OPEN_MODAL,
+} from "./userTypes";
 
 const initialState = {
-  user: {},
-  photos: [],
-  collections: [],
-  isLoading: false,
-  page: 1,
-  perPage: 10,
-  hasMore: true,
-  index: -1,
-  error: null,
+	user: {},
+	photos: [],
+	collections: [],
+	isLoading: false,
+	page: 1,
+	perPage: 5,
+	hasMore: true,
+	index: -1,
+	error: null,
 };
 
 const userReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case USER_FETCH_USER_PENDING:
-      return {
-        ...state,
-        isLoading: true,
-        error: null,
-      };
-    case USER_FETCH_USER_SUCCESS:
-      return {
-        ...state,
-        user: action.payload,
-        hasMore:!!action.payload.length,
-        isLoading: false,
-        error: null,
-      };
-    case USER_FETCH_USER_ERROR:
-      return {
-        ...state,
-        isLoading: true,
-        error: action.payload,
-      };
-    case USER_FETCH_USER_PHOTOS_PENDING:
-      return {
-        ...state,
-        isLoading: true,
-        error: null,
-      };
-    case USER_FETCH_USER_PHOTOS_SUCCESS:
-      return {
-        ...state,
-        photos: [...state.photos, ...action.payload],
-        isLoading: false,
-        page: state.page + 1,
-        hasMore: !!action.payload.length,
-        error: null,
-      };
-    case USER_FETCH_USER_PHOTOS_ERROR:
-      return {
-        ...state,
-        isLoading: false,
-        error: action.payload,
-      };
-    case USER_FETCH_USER_COLLECTIONS_PENDING:
-      return {
-        ...state,
-        isLoading: true,
-      };
-    case USER_FETCH_USER_COLLECTIONS_SUCCESS:
-      return {
-        ...state,
-        collections: [...state.collections, ...action.payload],
-        isLoading:false,
-        hasMore: !!action.payload.length,
-      };
-    case USER_FETCH_USER_COLLECTIONS_ERROR:
-      return {
-        ...state,
-        isLoading: false,
-        error: action.payload,
-      };
-    case USER_CLEAR_USER_PHOTOS:
-      return {
-        ...state,
-        photos: [],
-      };
-    case OPEN_MODAL:
-      return {
-        ...state,
-        index: action.payload,
-      };
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case USER_FETCH_USER_PENDING:
+			return {
+				...state,
+				isLoading: true,
+				error: null,
+			};
+		case USER_FETCH_USER_SUCCESS:
+			return {
+				...state,
+				user: action.payload,
+				hasMore: !!action.payload.length,
+				isLoading: false,
+				error: null,
+			};
+		case USER_FETCH_USER_ERROR:
+			return {
+				...state,
+				isLoading: true,
+				error: action.payload,
+			};
+		case USER_FETCH_USER_PHOTOS_PENDING:
+			return {
+				...state,
+				isLoading: true,
+				error: null,
+			};
+		case USER_FETCH_USER_PHOTOS_SUCCESS:
+			return {
+				...state,
+				photos: [...state.photos, ...action.payload],
+				isLoading: false,
+				page: state.page + 1,
+				hasMore: !!action.payload.length,
+				error: null,
+			};
+		case USER_FETCH_USER_PHOTOS_ERROR:
+			return {
+				...state,
+				isLoading: false,
+				error: action.payload,
+			};
+		case USER_FETCH_USER_COLLECTIONS_PENDING:
+			return {
+				...state,
+				isLoading: true,
+			};
+		case USER_FETCH_USER_COLLECTIONS_SUCCESS:
+			return {
+				...state,
+				collections: [...state.collections, ...action.payload],
+				isLoading: false,
+				hasMore: !!action.payload.length,
+			};
+		case USER_FETCH_USER_COLLECTIONS_ERROR:
+			return {
+				...state,
+				isLoading: false,
+				error: action.payload,
+			};
+		case USER_CLEAR_USER_PHOTOS:
+			return {
+				...state,
+				photos: [],
+			};
+		case OPEN_MODAL:
+			return {
+				...state,
+				index: action.payload,
+			};
+		default:
+			return state;
+	}
 };
-export const getPage = (state) => state.photos.page;
-export const getPerPage = (state) => state.photos.perPage;
+export const getPage = (state) => state.user.page;
+export const getPerPage = (state) => state.user.perPage;
 export default userReducer;
