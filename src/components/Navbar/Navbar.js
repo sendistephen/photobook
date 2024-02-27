a0a431a941fe87b7d2f133e6a0e95ccf2cd0f7fb
sendistephen <sendi.stev@gmail.com>
2024-02-27 19:55:50 +0300

    Remove unused import

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useAuth0 } from '@auth0/auth0-react';
 import {
   FormGroup,
