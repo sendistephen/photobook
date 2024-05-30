7fa00bd4b629826e7c91f20eb2ac5793f8a8edb2
sendistephen <sendi.stev@gmail.com>
2024-05-30 15:11:22 +0300

    [refactor] Refactor App component - Created custom hook `useAuthState` to manage authentication state. - Extracted routing into a new AppRoutes component. - Updated `App.jsx` to use `useAuthState` and `AppRoutes`. - Improved code organization and readability for better maintainability and testability. - Fix duplicate key issue iin PhotoList component.

---
+++ src/useAuthState.js
@@ -0,0 +1,28 @@
+import { getAuth, onAuthStateChanged } from '@firebase/auth';
+import { clearUser, setUser } from '@/store/authSlice';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+export const useAuthState = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        dispatch(
+          setUser({
+            uid: currentUser.uid,
+            displayName: currentUser.displayName,
+            email: currentUser.email,
+            photoURL: currentUser.photoURL,
+          })
+        );
+      } else {
+        dispatch(clearUser());
+      }
+    });
+
+    return () => unsubscribe();
+  }, [dispatch]);
+};
