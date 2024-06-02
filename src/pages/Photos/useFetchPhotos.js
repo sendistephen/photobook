0f3ed4d3557b59d432b62d1288d60d5fd9bd1f8f
sendistephen <sendi.stev@gmail.com>
2024-06-02 23:26:06 +0300

    Refactor usePhotoList hook to reduce complexity and improve readability
    - Extracted modal management logic to useModalManagement hook
    - Extracted photo fetching logic to useFetchPhotos hook
    - Combined extracted hooks in usePhotoList

---
+++ src/pages/Photos/useFetchPhotos.js
@@ -0,0 +1,31 @@
+import { throttle } from 'lodash';
+import { useCallback, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import {
+  fetchPhotos,
+  selectHasMore,
+  selectIsLoading,
+  selectPhotos,
+} from '@/store/photosSlice';
+
+export const useFetchPhotos = () => {
+  const dispatch = useDispatch();
+  const photos = useSelector(selectPhotos);
+  const hasMore = useSelector(selectHasMore);
+  const isLoading = useSelector(selectIsLoading);
+  const isBottomLoader = true;
+
+  useEffect(() => {
+    dispatch(fetchPhotos());
+  }, [dispatch]);
+
+  const fetchMorePhotos = useCallback(
+    throttle(() => {
+      dispatch(fetchPhotos());
+    }, 3000),
+    [dispatch],
+  );
+
+  return { photos, hasMore, isLoading, fetchMorePhotos, isBottomLoader };
+};
