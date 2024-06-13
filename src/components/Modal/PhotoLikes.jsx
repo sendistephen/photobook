9f33804e45f0f6ebce400a189d86fe6c335568c4
sendistephen <sendi.stev@gmail.com>
2024-06-13 23:58:50 +0300

    small fix

--- src/components/Modal/PhotoLikes.jsx
+++ src/components/Modal/PhotoLikes.jsx
@@ -1,6 +1,8 @@
 import heartIcon from '@/assets/icons/heart.svg';
 import { IconWrapper } from '@/pages/Photo/Photo.styles';
 
+import { Icon } from './Modal.styles';
+
 export const PhotoLikes = ({ likes }) => (
   <IconWrapper>
     <Icon src={heartIcon} alt="heart icon" />
