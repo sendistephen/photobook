06fe0ff5d16492cd50c8c88c3b83ed5e1c3dc9df
sendistephen <sendi.stev@gmail.com>
2024-09-16 22:00:36 +0300

    Refactor: Replace useUserLikes, useUserPhotos, and useUserCollections with a unified useUserData hook

--- src/components/User/UserCollections.tsx
+++ src/components/User/UserCollections.tsx
@@ -1,22 +1,25 @@
 import useOpenModal from '@/components/PhotoModal/useOpenModal';
 import { useParams } from 'react-router-dom';
 import Skeletons from '../Skeletons';
-import useUserCollections from './useUserCollections';
 import { PhotoGrid } from '../Common/PhotoGrid';
 import { imageExtractor } from '@/utils/helper';
+import { useUserData } from './useUserData';
 
 
 const UserCollections = () => {
   const { username } = useParams<{ username: string }>();
   const openModal = useOpenModal();
-  const query = useUserCollections({ username: username! });
+  const query = useUserData({
+    username: username!,
+    endpoint: 'collections'
+  });
 
   if (query.isLoading) return <Skeletons count={12} />;
   if (query.error) return <div>Error: {query.error.message}</div>;
 
   return (
     <PhotoGrid
-      photos={query.data ? query.data.pages.flat() : []}
+      photos={query.data ? query.data.pages.flat() as Collection[] : []}
       fetchNextPage={query.fetchNextPage}
       hasMore={!!query.hasNextPage}
       openModal={openModal}
